533b3d7e8b116426fdf0dc7dd612b3f89a9b8645
Josh <Josh@MBP-van-MacBook.hub>
2022-02-03 18:18:39 +1300

    worked on custom theme and about page

--- wp-content/themes/lotje-photography-theme/functions.php
+++ wp-content/themes/lotje-photography-theme/functions.php
@@ -1,9 +1,19 @@
 <?php
 /* enqueue scripts and style from parent theme */
     
-function twentytwentytwo_styles() {
-    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
-    array( 'twenty-twenty-two-style' ), wp_get_theme()->get( 'Version' ) );
-}
+// function twentytwentytwo_styles() {
+//     wp_enqueue_style( 'child-style', get_stylesheet_uri(),
+//     array( 'twenty-twenty-two-style' ), wp_get_theme()->get( 'Version' ) );
+// }
 
-add_action( 'wp_enqueue_scripts', 'twentytwentytwo_styles');
+// add_action( 'wp_enqueue_scripts', 'twentytwentytwo_styles');
+
+function my_theme_enqueue_styles() {
+    wp_register_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( 'parent-style' ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
