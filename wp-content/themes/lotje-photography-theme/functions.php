cc07315205ba1cb7c93ffc325eaa0b9fed703ed6
Josh <Josh@MBP-van-MacBook.hub>
2022-02-10 16:13:56 +1300

    redid search bar and SEO

--- wp-content/themes/lotje-photography-theme/functions.php
+++ wp-content/themes/lotje-photography-theme/functions.php
@@ -20,3 +20,14 @@ add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
 // Remove the category count for WooCommerce categories
 add_filter( 'woocommerce_subcategory_count_html', '__return_null' );
+
+// change search placeholder
+function html5_search_form( $form ) { 
+     $form = '<section class="search"><form role="search" method="get" id="search-form" action="' . home_url( '/' ) . '" >
+    <label class="screen-reader-text" for="s">' . __('',  'domain') . '</label>
+     <input type="search" value="' . get_search_query() . '" name="s" id="s" placeholder="search products..." />
+     <input type="submit" id="searchsubmit" value="'. esc_attr__('Go', 'domain') .'" />
+     </form></section>';
+     return $form;
+}
+ add_filter( 'get_search_form', 'html5_search_form' );
